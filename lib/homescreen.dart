585aa94e46e159a6010a60ad84393c031db6cad5
vishalvinayram5432@gmail.com <vishalvinayram5432@gmail.com>
2024-07-02 08:53:52 +0530

    some minor changes in the home screen .

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -8,12 +8,14 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
   List<Photo> _photos = [];
+  late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: 2, vsync: this);
     _loadPhotos();
   }
 
@@ -30,15 +32,23 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      body: _photos.isEmpty
-          ? Center(child: Text('No photos yet'))
-          : ListView(
-              children: [
-                _buildCategorySection('Food'),
-                _buildCategorySection('Exercise'),
-              ],
-            ),
+      appBar: AppBar(
+        title: Text('Home'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: 'Food'),
+            Tab(text: 'Exercise'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          _buildCategoryView('Food'),
+          _buildCategoryView('Exercise'),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pushNamed(context, '/capture');
@@ -48,32 +58,55 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _buildCategorySection(String category) {
+  Widget _buildCategoryView(String category) {
     final categoryPhotos = _photos.where((p) => p.category == category).toList();
-    return ExpansionTile(
-      title: Text(category),
-      children: categoryPhotos.map((photo) => _buildPhotoTile(photo)).toList(),
-    );
+    return categoryPhotos.isEmpty
+        ? Center(child: Text('No photos yet'))
+        : GridView.builder(
+            padding: const EdgeInsets.all(8),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8,
+            ),
+            itemCount: categoryPhotos.length,
+            itemBuilder: (context, index) {
+              return _buildPhotoTile(categoryPhotos[index]);
+            },
+          );
   }
 
   Widget _buildPhotoTile(Photo photo) {
-    return ListTile(
-      leading: Image.memory(base64Decode(photo.image)),
-      title: Text(photo.note),
-      subtitle: Column(
+    return Card(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-
-          if (photo.category == 'Food') ...[
-            Text('Food Name: ${photo.foodName}'),
-            Text('Time: ${photo.foodTime}'),
-            Text('Quantity: ${photo.quantity} ${photo.foodType == 'drinks' ? 'ml' : 'gm'}'),
-          ],
-          if (photo.category == 'Exercise') ...[
-            Text('Exercise: ${photo.exerciseName}'),
-            Text('Reps: ${photo.reps}'),
-            Text('Weight: ${photo.weight} kg'),
-          ],
+          Expanded(
+            child: Image.memory(
+              base64Decode(photo.image),
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(photo.note, style: TextStyle(fontWeight: FontWeight.bold)),
+                if (photo.category == 'Food') ...[
+                  Text('Food Name: ${photo.foodName}'),
+                  Text('Time: ${photo.foodTime}'),
+                  Text('Quantity: ${photo.quantity} ${photo.foodType == 'drinks' ? 'ml' : 'gm'}'),
+                ],
+                if (photo.category == 'Exercise') ...[
+                  Text('Exercise: ${photo.exerciseName}'),
+                  Text('Reps: ${photo.reps}'),
+                  Text('Weight: ${photo.weight} kg'),
+                ],
+              ],
+            ),
+          ),
         ],
       ),
     );
